342d5f82b0257726a27156c8ce8330dbf029de09
Mike Auty <m@auty.ca>
2021-11-10 19:08:48 +0000

    modified server to serve static public folder for custom.css

--- express_server.js
+++ express_server.js
@@ -16,6 +16,7 @@ app.use(cookieParser())
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// app.use(express.static('/public'));
 app.use(express.static('public'));
 
 
